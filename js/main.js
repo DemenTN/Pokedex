f8cd90084200f1bb15fb4d92b3b64a5026c9088b
Nick <94718670+DemenTN@users.noreply.github.com>
2023-04-27 12:19:14 -0700

    Made code less dry
    Created a function to handle the stat bars.
    Changed to async/await syntax

--- js/main.js
+++ js/main.js
@@ -1,57 +1,30 @@
-//Example fetch using pokemonapi.co
+//Simple pokedex using pokemonapi.co API
+//could use a try catch
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
+async function getFetch() {
   const choice = document.querySelector('input').value
   const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+  const res = await fetch(url)
+  const data = await res.json()
+  console.log(data)
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        document.querySelector('img').src=data.sprites.front_default
-        document.querySelector('h2').innerText=data.name
+  document.querySelector('img').src=data.sprites.front_default
+  document.querySelector('h2').innerText=data.name
 
-        document.querySelector('.hp').innerText=data.stats[0].base_stat
-        let hpValue=data.stats[0].base_stat
-        let hpTag = document.getElementById('hpBar')
-        hpTag.style.width = (hpValue*2) + 'px'
-        statBarColorPicker(hpValue,hpTag)
-       
-        document.querySelector('.att').innerText=data.stats[1].base_stat
-        let attValue=data.stats[1].base_stat
-        let attTag = document.getElementById('attBar')
-        attTag.style.width = (attValue*2) + 'px'
-        statBarColorPicker(attValue,attTag)
+  function statBarMaker(statClass,statAPIValue,statId) {
+    document.querySelector(statClass).innerText=statAPIValue
+    let statTag = document.getElementById(statId)
+    statTag.style.width = (statAPIValue*2) + 'px'
+    statBarColorPicker(statAPIValue,statTag)
+  }
 
-        document.querySelector('.def').innerText=data.stats[2].base_stat
-        let defValue=data.stats[2].base_stat
-        let defTag = document.getElementById('defBar')    
-        defTag.style.width = (defValue*2) + 'px'
-        statBarColorPicker(defValue,defTag)
-
-        document.querySelector('.spa').innerText=data.stats[3].base_stat
-        let spaValue=data.stats[3].base_stat
-        let spaTag = document.getElementById('spaBar')
-        spaTag.style.width = (spaValue*2) + 'px'
-        statBarColorPicker(spaValue,spaTag)
-
-        document.querySelector('.spd').innerText=data.stats[4].base_stat
-        let spdValue=data.stats[4].base_stat
-        let spdTag = document.getElementById('spdBar')
-        spdTag.style.width = (spdValue*2) + 'px'
-        statBarColorPicker(spdValue,spdTag)
-
-        document.querySelector('.spe').innerText=data.stats[5].base_stat
-        let speValue=data.stats[5].base_stat
-        let speTag = document.getElementById('speBar')
-        speTag.style.width = (speValue*2) + 'px'
-        statBarColorPicker(speValue,speTag)
-
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+  statBarMaker('.hp',data.stats[0].base_stat,'hpBar')
+  statBarMaker('.att',data.stats[1].base_stat,'attBar')
+  statBarMaker('.def',data.stats[2].base_stat,'defBar')
+  statBarMaker('.spa',data.stats[3].base_stat,'spaBar')
+  statBarMaker('.spd',data.stats[4].base_stat,'spdBar')
+  statBarMaker('.spe',data.stats[5].base_stat,'speBar')
 }
 
 function statBarColorPicker(statValue,statTag){
